82cd90d93b17d84d4ca0aa4c2935b0268801aad5
Meghnath <msparkhe@gmail.com>
2022-12-27 21:23:34 +0530

    Add files

---
+++ strings_psets/PSET3/solution/solution_1.py
@@ -0,0 +1,6 @@
+''' Capture the USN & convert all lowercase to uppercase
+'''
+
+usn = input("Please enter USN\n")  # take input
+usn_upper = usn.upper()  # Convert lowercase to Uppercase
+print(usn_upper)  # print
