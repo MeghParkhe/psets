d333fd4eeff22c7c1d8227805107397e487732fc
Meghnath <msparkhe@gmail.com>
2023-01-06 21:32:59 +0530

    PSET-1 is added with solution

---
+++ strings_psets/PSET-1/PSET-1_solution.py
@@ -0,0 +1,5 @@
+"""  Solution for PSET-1"""
+''' Write a python script that reads an input from the std. input and prints the same in the std. output.'''
+
+Name = input("Enter your Name:\n")  #take input from user
+print("Welcome ",Name)  # Print the input taken with message
